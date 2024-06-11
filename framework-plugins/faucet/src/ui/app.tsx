be950e4869066aa2eebe0284437364c30cfe0e4a
corbifex <sander@moosty.com>
2024-06-11 10:48:20 +0200

    Faucet version bump

--- framework-plugins/faucet/src/ui/app.tsx
+++ framework-plugins/faucet/src/ui/app.tsx
@@ -204,7 +204,7 @@ export const App: React.FC = () => {
 				</div>
 			</section>
 			<footer>
-				<p className={styles.copyright}>© 2023 Lisk Foundation</p>
+				<p className={styles.copyright}>© 2024 Klayr Labs</p>
 			</footer>
 		</div>
 	);
